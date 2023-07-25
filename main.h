7a7f7ed9fd349b70dcf2be2f5490248e143e7a08
zeyadAbdo22 <zeyad.abdo73@gmail.com>
2023-07-25 15:41:20 -0700

    printf

--- main.h
+++ main.h
@@ -1,13 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
-int _putchar(char c);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_number(va_list args);
-int print_binary(va_list args);
+/**
+* struct cnvert - structure  functions with symbols
+*
+* @op: opereration
+* @funct: function assessment
+*/
+struct cnvert
+{
+	char *op;
+	int (*funct)(va_list);
+};
+typedef struct cnvert cnvert_t;
+
+int pars(const char *form, cnvert_t f_lst[], va_list arg_lst);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(va_list);
+int _print_strng(va_list);
+int _print_percent(va_list);
+
 
 #endif
